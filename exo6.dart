b87d67ced0e1c777d67e1f772260e6c9f4578453
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-15 02:25:46 -0500

    nouvelle modif

--- exo6.dart
+++ exo6.dart
@@ -2,3 +2,14 @@ class Connectable{
 void connecter(String utilisateur);
   void deconnecter();
 }
+class ServeurAPI implements Connectable{ 
+  @override
+  void connecter(String utilisateur){ 
+    print("ServeurAPI:connexion etablie pour $utilisateur"); 
+  }
+
+  @override
+  void deconnecter() {
+    print("serveurAPI:deconnexion réussie"); 
+  }
+}
