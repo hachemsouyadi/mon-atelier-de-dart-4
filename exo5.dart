5ddfd9dcdcd20b9c61c81ed042cc15f724a9d1a2
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-15 02:09:08 -0500

    nouvelle modif

--- exo5.dart
+++ exo5.dart
@@ -3,6 +3,11 @@ abstract class Forme{
     void afficherMessage(){
     print("Calcul de l'aire...");
   }
-
-
    }
+   class Cercle extends Forme {
+  final double rayon;
+  Cercle(this.rayon);
+  @override
+  double calculerAire(){
+    return 3.14*rayon*rayon;
+  }}
