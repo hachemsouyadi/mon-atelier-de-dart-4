9913e5763144b3bc46c4e437504b23f5630c7761
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-15 01:29:36 -0500

    nouvelle modif

--- exo3.dart
+++ exo3.dart
@@ -3,4 +3,7 @@ class Compte{
   final String numeroCompte;
   Compte(this.numeroCompte,[double solde = 0.0]) :_solde=solde;
   String get solde=>"${_solde.toStringAsFixed(2)}\$";
+  void depot(double montant){
+    if (montant>0){
+    _solde+=montant;}}
   }
