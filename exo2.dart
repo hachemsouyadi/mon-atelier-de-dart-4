8516f66cf2ba27c922e5326c0a8d2ef333d03ee7
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-15 01:16:12 -0500

    nouvelle modif

--- exo2.dart
+++ exo2.dart
@@ -2,10 +2,16 @@ class Point{
   double _x;
   double _y;
   Point(this._x, this._y);
-  void move(double dx, double dy)
+  void move(double dx, double dy){
    _x = _x + dx;
    _y = _y + dy;
   }
   void display(){
     print("$_x,$_y");
-  }}
+  }
+  void main(){
+ var pt = Point(2, 5);
+ pt.display();
+ pt.move(-1, 3);
+ pt.display();
+}}
